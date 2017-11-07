73fddde65a4651773201a66d8d69d85a0cfe53e7
Reynald <reynald_liandra@hotmail.com>
2017-11-07 13:57:48 +0700

    Update FixedSizeView.js
    fixed typos

--- src/FixedSizeView.js
+++ src/FixedSizeView.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react'
-import PropTypes from 'prop-types
+import PropTypes from 'prop-types'
 import { View } from 'react-native'
 
 class FixedSizeView extends Component {
